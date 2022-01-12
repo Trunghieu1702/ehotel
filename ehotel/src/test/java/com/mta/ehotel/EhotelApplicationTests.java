dd7efc7b794dfc685bec57c4b04cf370b6e3fcb6
hypvn_partner07@hyperion.co.jp <hyperion-mec>
2022-01-12 18:30:56 +0700

    createNew

---
+++ ehotel/src/test/java/com/mta/ehotel/EhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mta.ehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
